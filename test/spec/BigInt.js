ca74f9783fb61d673390507809a9408a3fe9ca8b
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-06 18:15:20 -0700

    Fixed patching

--- test/spec/BigInt.js
+++ test/spec/BigInt.js
@@ -5,8 +5,6 @@ var mock = require('mock-require');
 var JSBI_TESTS = path.dirname(require.resolve('jsbi-tests'));
 var METHODS = ['tests', 'as-int-n'];
 
-require('../../lib/patch');
-
 describe('BigInt Exists', function () {
   describe('BigInt', function () {
     var JSBI = require('../../lib/BigInt');
@@ -16,34 +14,33 @@ describe('BigInt Exists', function () {
         mock('jsbi', JSBI);
         require(JSBI_TESTS + '/tests/' + method);
       });
+    });
 
-      it('supports isBigInt', function () {
-        assert.equal(JSBI.isBigInt(false), false);
-        assert.equal(JSBI.isBigInt(true), false);
-        assert.equal(JSBI.isBigInt(1), false);
-        assert.equal(JSBI.isBigInt('hello'), false);
-        assert.equal(JSBI.isBigInt(JSBI.BigInt(1)), true);
-      });
+    it('supports isBigInt', function () {
+      assert.equal(JSBI.isBigInt(false), false);
+      assert.equal(JSBI.isBigInt(true), false);
+      assert.equal(JSBI.isBigInt(1), false);
+      assert.equal(JSBI.isBigInt('hello'), false);
+      assert.equal(JSBI.isBigInt(JSBI.BigInt(1)), true);
     });
   });
 
   describe('JTBI', function () {
-    require('../../lib/patch');
-    var JSBI = require('jsbi/dist/jsbi-umd.js');
+    var JSBI = require('../../lib/jsbi');
 
     METHODS.forEach(function (method) {
       it(method, function () {
         mock('jsbi', JSBI);
         require(JSBI_TESTS + '/tests/' + method);
       });
+    });
 
-      it('supports isBigInt', function () {
-        assert.equal(JSBI.isBigInt(false), false);
-        assert.equal(JSBI.isBigInt(true), false);
-        assert.equal(JSBI.isBigInt(1), false);
-        assert.equal(JSBI.isBigInt('hello'), false);
-        assert.equal(JSBI.isBigInt(JSBI.BigInt(1)), true);
-      });
+    it('supports isBigInt', function () {
+      assert.equal(JSBI.isBigInt(false), false);
+      assert.equal(JSBI.isBigInt(true), false);
+      assert.equal(JSBI.isBigInt(1), false);
+      assert.equal(JSBI.isBigInt('hello'), false);
+      assert.equal(JSBI.isBigInt(JSBI.BigInt(1)), true);
     });
   });
 
@@ -55,14 +52,14 @@ describe('BigInt Exists', function () {
         mock('jsbi', JSBI);
         require(JSBI_TESTS + '/tests/' + method);
       });
+    });
 
-      it('supports isBigInt', function () {
-        assert.equal(JSBI.isBigInt(false), false);
-        assert.equal(JSBI.isBigInt(true), false);
-        assert.equal(JSBI.isBigInt(1), false);
-        assert.equal(JSBI.isBigInt('hello'), false);
-        assert.equal(JSBI.isBigInt(JSBI.BigInt(1)), true);
-      });
+    it('supports isBigInt', function () {
+      assert.equal(JSBI.isBigInt(false), false);
+      assert.equal(JSBI.isBigInt(true), false);
+      assert.equal(JSBI.isBigInt(1), false);
+      assert.equal(JSBI.isBigInt('hello'), false);
+      assert.equal(JSBI.isBigInt(JSBI.BigInt(1)), true);
     });
   });
 });
