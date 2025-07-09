b30eedf9bb84fed2af0320a60cd0b76d71763b63
Kevin Malakoff <kmalakoff@gmail.com>
2025-07-09 07:24:08 -0700

    Comprehensive types

--- src/jsbi.ts
+++ src/jsbi.ts
@@ -1,16 +1,16 @@
 // patch missing methods
 if (typeof Math.clz32 === 'undefined') Math.clz32 = require('clz32');
 if (typeof Math.imul === 'undefined') Math.imul = require('imul');
+// @ts-ignore
 if (typeof Number.MAX_SAFE_INTEGER === 'undefined') Number.MAX_SAFE_INTEGER = require('max-safe-integer');
-// eslint-disable-next-line no-extend-native
 if (typeof String.prototype.charCodeAt === 'undefined') String.prototype.charCodeAt = require('char-code-at');
 if (typeof Object.setPrototypeOf === 'undefined') Object.setPrototypeOf = require('setprototypeof');
 
-var JSBI = require('jsbi/dist/jsbi-umd.js');
+import JSBI from 'jsbi/dist/jsbi-umd.js';
 
 // jsbi extension
 JSBI.isBigInt = function isBigInt(x) {
   return x instanceof JSBI;
 };
 
-module.exports = JSBI;
+export default JSBI;
