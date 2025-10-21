a4e4c46831b1230f63af4ffd68549c492eef47d2
Kevin Malakoff <kmalakoff@gmail.com>
2025-10-21 00:02:55 -0700

    Add publish workflow and update tool configurations

--- src/jsbi.ts
+++ src/jsbi.ts
@@ -1,8 +1,9 @@
 // patch missing methods
 if (typeof Math.clz32 === 'undefined') Math.clz32 = require('clz32');
 if (typeof Math.imul === 'undefined') Math.imul = require('imul');
-// @ts-ignore
-if (typeof Number.MAX_SAFE_INTEGER === 'undefined') Number.MAX_SAFE_INTEGER = require('max-safe-integer');
+if (typeof Number.MAX_SAFE_INTEGER === 'undefined')
+  // @ts-expect-error
+  Number.MAX_SAFE_INTEGER = require('max-safe-integer');
 if (typeof String.prototype.charCodeAt === 'undefined') String.prototype.charCodeAt = require('char-code-at');
 if (typeof Object.setPrototypeOf === 'undefined') Object.setPrototypeOf = require('setprototypeof');
 
