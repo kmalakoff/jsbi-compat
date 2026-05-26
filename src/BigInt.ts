2cec4a195bf7c799c9277fb45cd0cd838116f917
Kevin Malakoff <kmalakoff@gmail.com>
2026-05-26 08:56:20 -0700

    Fix broken types

--- src/BigInt.ts
+++ src/BigInt.ts
@@ -1,93 +1,93 @@
 const JSBI = {
-  BigInt(value) {
+  BigInt(value: bigint | number | string | boolean) {
     return BigInt(value);
   },
-  add(a, b) {
+  add(a: bigint, b: bigint) {
     return a + b;
   },
-  subtract(a, b) {
+  subtract(a: bigint, b: bigint) {
     return a - b;
   },
-  multiply(a, b) {
+  multiply(a: bigint, b: bigint) {
     return a * b;
   },
-  divide(a, b) {
+  divide(a: bigint, b: bigint) {
     return a / b;
   },
-  remainder(a, b) {
+  remainder(a: bigint, b: bigint) {
     return a % b;
   },
-  exponentiate(a, b) {
+  exponentiate(a: bigint, b: bigint) {
     return a ** b;
   },
-  unaryMinus(a) {
+  unaryMinus(a: bigint) {
     return -a;
   },
-  bitwiseNot(a) {
+  bitwiseNot(a: bigint) {
     return ~a;
   },
-  leftShift(a, b) {
+  leftShift(a: bigint, b: bigint) {
     return a << b;
   },
-  signedRightShift(a, b) {
+  signedRightShift(a: bigint, b: bigint) {
     return a >> b;
   },
-  bitwiseAnd(a, b) {
+  bitwiseAnd(a: bigint, b: bigint) {
     return a & b;
   },
-  bitwiseOr(a, b) {
+  bitwiseOr(a: bigint, b: bigint) {
     return a | b;
   },
-  bitwiseXor(a, b) {
+  bitwiseXor(a: bigint, b: bigint) {
     return a ^ b;
   },
-  equal(a, b) {
+  equal(a: bigint, b: bigint) {
     return a === b;
   },
-  notEqual(a, b) {
+  notEqual(a: bigint, b: bigint) {
     return a !== b;
   },
-  lessThan(a, b) {
+  lessThan(a: bigint, b: bigint) {
     return a < b;
   },
-  lessThanOrEqual(a, b) {
+  lessThanOrEqual(a: bigint, b: bigint) {
     return a <= b;
   },
-  greaterThan(a, b) {
+  greaterThan(a: bigint, b: bigint) {
     return a > b;
   },
-  greaterThanOrEqual(a, b) {
+  greaterThanOrEqual(a: bigint, b: bigint) {
     return a >= b;
   },
-  EQ(x, y) {
+  EQ(x: bigint, y: bigint) {
     return x === y;
   },
-  NE(x, y) {
+  NE(x: bigint, y: bigint) {
     return x !== y;
   },
-  LT(x, y) {
+  LT(x: bigint, y: bigint) {
     return x < y;
   },
-  LE(x, y) {
+  LE(x: bigint, y: bigint) {
     return x <= y;
   },
-  GT(x, y) {
+  GT(x: bigint, y: bigint) {
     return x > y;
   },
-  GE(x, y) {
+  GE(x: bigint, y: bigint) {
     return x >= y;
   },
-  toNumber(a) {
+  toNumber(a: bigint) {
     return Number(a);
   },
-  asIntN(n, x) {
+  asIntN(n: number, x: bigint) {
     return BigInt.asIntN(n, x);
   },
-  asUintN(n, x) {
+  asUintN(n: number, x: bigint) {
     return BigInt.asUintN(n, x);
   },
   // jsbi extension
-  isBigInt(x) {
+  isBigInt(x: unknown) {
     return typeof x === 'bigint';
   },
 };
